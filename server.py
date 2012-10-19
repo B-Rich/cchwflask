7a28354453a2ba914df0f82582819d0547fc4e11
Valentin Haenel <valentin.haenel@gmx.de>
2012-10-19 10:01:17 +0200

    slightly extended hello-world example

---
+++ server.py
@@ -0,0 +1,29 @@
+import os
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello():
+    return 'Hello World!'
+
+@app.route('/create')
+def create():
+    return "created"
+
+@app.route('/update')
+def update():
+    return "updated"
+
+@app.route('/delete')
+def delete():
+    return "deleted"
+
+@app.route('/health-check')
+def health_check():
+    return "All your cloud are belong to us!"
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
